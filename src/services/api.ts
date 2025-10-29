93c83ade3dd567dc885a72f118d95c501202452c
Rafael Silverio <fael_st@hotmail.com>
2025-10-29 10:47:35 -0300

    fix: correct env

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const api = axios.create({
-  baseURL: process.env.API_URL || "http://10.222.200.87:3000", // altere para sua URL de API
+  baseURL: process.env.API_BASE_URL || "http://10.222.200.87:3000", // altere para sua URL de API
   timeout: 15000,
 });
 
